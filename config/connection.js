af43853e2468a23434b6a8228526dd7499f5e680
tcuddle <thcrump38@yahoo.com>
2019-01-26 11:32:41 -0600

    Changed reference to ClearDB db url

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (process.env.CLEARDB_DATABASE_URL) {
+  connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
 } else {
   connection = mysql.createConnection({
   port: 8889,
