4c3aa2cdddf3adce960b6d10ec611fcc87207eba
tcuddle <thcrump38@yahoo.com>
2019-01-26 11:08:53 -0600

    Switch line order in server

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+var app = express();
 var PORT = process.env.PORT || 3000;
 
-var app = express();
 
 
 app.use(express.static("public"));
